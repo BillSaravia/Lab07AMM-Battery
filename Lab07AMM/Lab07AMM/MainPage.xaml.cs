671ae95c6b65bbaf2aed2151afdd11f8e8dfb93a
Bill Saravia <bill.saravia@tecsup.edu.pe>
2023-05-21 19:51:57 -0500

    Agregar archivos de proyecto.

---
+++ Lab07AMM/Lab07AMM/MainPage.xaml.cs
@@ -0,0 +1,19 @@
+﻿using Lab07AMM.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace Lab07AMM
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
